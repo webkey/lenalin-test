4da7918fcb96f128baefc92cbd084d645714da1a
webkey <serhiy_i@ukr.net>
2020-03-02 01:01:29 +0300

    Pages: news, news-one, about, contacts, 404, catalog

---
+++ app/blocks/popular-slider/popular-slider.js
@@ -0,0 +1,68 @@
+app.popularSlider = {
+	sliderElement: '.js-popular-slider',
+	init() {
+		app.common.initScript('swiper.min', 'Swiper', () => {
+			this.lazyLoad();
+			this.runSlider();
+		});
+		app.common.initStyle('swiper.min');
+	},
+	lazyLoad() {
+		const self = this;
+		app.common.initScript('lazyload.min', 'LazyLoad', () => {
+			app.lazyLoadNewsSliderInstance = new LazyLoad({
+				threshold: 50,
+				elements_selector: self.sliderElement + ' .lazy-load',
+				class_loaded: 'image-loaded'
+			});
+		});
+	},
+	runSlider() {
+		const $slider = $(this.sliderElement);
+
+		if ($slider.length) {
+			const slider = new Swiper(this.sliderElement, this.options);
+
+			slider.on('init', function () {
+				$slider.addClass('is-loaded');
+			});
+
+			slider.init();
+		}
+	},
+	options: {
+		init: false,
+		loop: true,
+		slidesPerView: 3,
+		slidesPerGroup: 3,
+		spaceBetween: 30,
+		watchSlidesVisibility: true,
+		lazy: true,
+
+		navigation: {
+			nextEl: '.js-popular-slider__button-next',
+			prevEl: '.js-popular-slider__button-prev'
+		},
+
+		breakpoints: {
+			767: {
+				slidesPerView: 2,
+				slidesPerGroup: 2,
+				spaceBetween: 20
+			},
+			575: {
+				slidesPerView: 1,
+				slidesPerGroup: 1,
+				spaceBetween: 16
+			},
+		},
+
+		on: {
+			slideChangeTransitionEnd() {
+				if (app.lazyLoadNewsSliderInstance) {
+					app.lazyLoadNewsSliderInstance.update();
+				}
+			}
+		}
+	}
+};
