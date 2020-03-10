cf99892b77a2d2caeeef88ee5f49dc640612f1ea
sergey.ilchenko <s.ilchenko@imedia.by>
2020-03-10 20:03:21 +0300

    Merge from lenalin (optimized svg icons)

--- app/blocks/product-card-gallery/product-card-gallery.js
+++ app/blocks/product-card-gallery/product-card-gallery.js
@@ -1,5 +1,6 @@
 app.productCardGallery = {
-	sliderElement: '.p-card-gallery-js',
+	sliderElement: '.js-pc-gallery',
+	fancyboxElement: '.js-pc-gallery-fancybox',
 	init() {
 		app.common.initScript('swiper.min', 'Swiper', () => {
 			this.lazyLoad();
@@ -19,124 +20,103 @@ app.productCardGallery = {
 	},
 	runSlider() {
 		const $slider = $(this.sliderElement);
+		const self = this;
 
 		if ($slider.length) {
-			const cardGalleryThumbsTpl = $('<div class="p-card-gallery-thumbs"><div class="p-card-gallery-thumbs__arrow-prev arrow-prev-js"></div><div class="swiper-container"><div class="swiper-wrapper"></div></div><div class="p-card-gallery-thumbs__arrow-next arrow-next-js"></div></div>');
+			const cardGalleryThumbsTpl = $(`<div class="product-card-gallery__thumbs">
+												<div class="product-card-gallery-thumbs">
+													<div class="product-card-gallery-thumbs__button product-card-gallery-thumbs__button_prev js-pc-gallery-thumbs__button-prev"><svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.22 8l3.3 3.3-.94.94L5.33 8l4.25-4.24.94.94L7.22 8z" fill="#000"></path></svg></div>
+													<div class="swiper-container">
+														<div class="swiper-wrapper"></div>
+													</div>
+													<div class="product-card-gallery-thumbs__button product-card-gallery-thumbs__button_next js-pc-gallery-thumbs__button-next"><svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.78 8l-3.3-3.3.94-.94L10.67 8l-4.25 4.24-.94-.94L8.78 8z" fill="#000"></path></svg></div>
+												</div>
+											</div>`);
 
 			$slider.each(function () {
 				const $curSlider = $(this);
-				const $imgList = $curSlider.find('.p-card-gallery-images-js');
-				const $imgListItem = $imgList.find('img').parent();
+				const $imagesSlider = $curSlider.find('.js-pc-gallery-images');
 
-				// create thumbs
-				$imgList.after(cardGalleryThumbsTpl.clone());
+				// Create thumbs
+				$imagesSlider.parent().after(cardGalleryThumbsTpl.clone());
 
-				const $galleryThumbs = $curSlider.find('.p-card-gallery-thumbs');
-				const $galleryThumbsArrPrev = $galleryThumbs.find('.arrow-prev-js');
-				const $galleryThumbsArrNext = $galleryThumbs.find('.arrow-next-js');
 
-				$.each($imgListItem, function () {
-					const $this = $(this);
-					$galleryThumbs.find('.swiper-wrapper').append($('<div class="swiper-slide p-card-gallery-thumbs__item"><img src="' + $this.find('img').attr('data-thumb') + '" alt="' + $this.find('img').attr('alt') + '"></div>'));
+				const $galleryThumbs = $curSlider.find('.product-card-gallery-thumbs');
+				const $galleryThumbsArrPrev = $galleryThumbs.find('.js-pc-gallery-thumbs__button-prev');
+				const $galleryThumbsArrNext = $galleryThumbs.find('.js-pc-gallery-thumbs__button-next');
+
+				$.each($imagesSlider.find('.swiper-slide'), function () {
+					const $curSlide = $(this);
+					$galleryThumbs.find('.swiper-wrapper').append($('<div class="swiper-slide product-card-gallery-thumbs__item" style="background-image: url(' + $curSlide.find('[data-thumb]').attr('data-thumb') + ')"></div>'));
 				});
 
-				const cardImgSlider = new Swiper($imgList, {
+				const thumbsSliderInstance = new Swiper($galleryThumbs.find('.swiper-container'), {
+					spaceBetween: 10,
+					slidesPerView: 'auto',
+					watchSlidesVisibility: true,
+					watchSlidesProgress: true,
+					navigation: {
+						prevEl: $galleryThumbsArrPrev,
+						nextEl: $galleryThumbsArrNext,
+					}
+				});
+
+				const imagesSliderInstance = new Swiper($imagesSlider, {
 					init: false,
-					spaceBetween: 20,
-					preloadImages: false,
-					lazy: {
-						loadPrevNext: true,
-						loadOnTransitionStart: true
-					},
+					spaceBetween: 10,
+					watchSlidesVisibility: true,
+					watchSlidesProgress: true,
 					thumbs: {
-						swiper: {
-							el: $galleryThumbs.find('.swiper-container'),
-							direction: 'vertical',
-							slidesPerView: 'auto',
-							spaceBetween: 14,
-							freeMode: true,
-							slideToClickedSlide: true,
-							watchSlidesVisibility: true,
-							watchSlidesProgress: true,
-							navigation: {
-								nextEl: $galleryThumbsArrNext,
-								prevEl: $galleryThumbsArrPrev,
-							},
-							breakpoints: {
-								767: {
-									direction: 'horizontal'
-								}
-							}
-						},
+						swiper: thumbsSliderInstance,
+					},
+					pagination: {
+						type: 'bullets',
+						el: '.js-pc-gallery-images__pagination',
+						clickable: true,
+						bulletClass: 'product-card-gallery-images__pagination-bullet',
+						bulletActiveClass: 'product-card-gallery-images__pagination-bullet_active',
+						renderBullet(index, className) {
+							return '<div class="' + className + '"><span></span></div>';
+						}
 					},
 					on: {
-						/*lazyImageReady: function (slide, image) {
-							objectFitImages($(image));
-						}*/
+						slideChangeTransitionEnd() {
+							if (app.lazyLoadNewsSliderInstance) {
+								app.lazyLoadNewsSliderInstance.update();
+							}
+						}
 					}
 				});
 
-				cardImgSlider.on('init', function () {
+				imagesSliderInstance.on('init', function () {
 					$curSlider.addClass('is-loaded');
-					// object-fit for non-support browsers
-					// objectFitImages($('img', $(cardImgSlider.el)));
 				});
 
-				cardImgSlider.init();
+				app.common.initScript('jquery.fancybox.min', 'fancybox', () => {
+					$().fancybox({
+						selector: '.js-pc-gallery-fancybox:visible',
+						closeExisting: true,
+						baseClass: 'fancybox-gallery',
+						buttons: [
+							'zoom',
+							'thumbs',
+							'close'
+						],
+						beforeClose(instance, current) {
+							imagesSliderInstance.slideTo(current.index);
+						}
+					});
+				});
+				app.common.initStyle('jquery.fancybox.min');
 
-				/*$().fancybox({
-					selector: '.p-card-gallery-js a:visible',
-					infobar: true,
-					baseClass: 'white-spaces',
-					buttons: [
-						"zoom",
-						//"share",
-						// "slideShow",
-						//"fullScreen",
-						//"download",
-						// "thumbs",
-						"close"
-					],
-					beforeClose(instance, current) {
-						cardImgSlider.slideTo(current.index);
-					}
-				});*/
+				imagesSliderInstance.init();
 			});
-		}
-	},
-	options: {
-		init: false,
-		loop: true,
-		slidesPerView: 3,
-		slidesPerGroup: 3,
-		spaceBetween: 30,
-		watchSlidesVisibility: true,
-		lazy: true,
 
-		navigation: {
-			nextEl: '.js-news-slider__button-next',
-			prevEl: '.js-news-slider__button-prev'
-		},
-
-		breakpoints: {
-			767: {
-				slidesPerView: 2,
-				slidesPerGroup: 2,
-				spaceBetween: 20
-			},
-			575: {
-				slidesPerView: 1,
-				slidesPerGroup: 1,
-				spaceBetween: 16
-			},
-		},
-
-		on: {
-			slideChangeTransitionEnd() {
-				if (app.lazyLoadNewsSliderInstance) {
-					app.lazyLoadNewsSliderInstance.update();
-				}
-			}
+			$('body').on('click', '.js-pc-gallery-images__zoom', function (e) {
+				const $curBtnZoom = $(this);
+				e.preventDefault();
+				$curBtnZoom.closest($slider).find('.swiper-slide-active').find(self.fancyboxElement).trigger('click');
+			});
 		}
 	}
 };
